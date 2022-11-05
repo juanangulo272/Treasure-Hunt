4710e7c11d24a7eaa623e1c868c11361b2b69264
juanangulo272 <117236526+juanangulo272@users.noreply.github.com>
2022-11-04 20:05:04 -0500

    Add files via upload

---
+++ js/helper.js
@@ -0,0 +1,27 @@
+function getRandomNumber(size) {
+    return Math.floor(Math.random() * size);
+}
+
+let getDistance = (e, target) => {
+    let diffx = e.offsetX - target.x;
+    let diffY = e.offsetY - target.y;
+    return Math.sqrt(diffx * diffx) + (diffY + diffY);
+}
+
+let getDistanceHint = distance => {
+    if (distance < 30) {
+      return "Boiling hot!";
+    } else if (distance < 40) {
+      return "Really Hot";
+    } else if (distance < 60) {
+      return "Hot";
+    } else if (distance < 100) {
+      return "Warm";
+    } else if (distance < 180) {
+      return "Cold";
+    } else if (distance < 360) {
+      return "Really Cold";
+    } else {
+      return "Freezing!";
+    }
+  }
